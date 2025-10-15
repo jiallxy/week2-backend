5858a4978654d309a247bed3cea77fdee1f349e0
Justin Luo <87254766+jiallxy@users.noreply.github.com>
2025-10-15 19:35:15 -0400

    1

--- api/main.py
+++ api/main.py
@@ -6,14 +6,7 @@ app = FastAPI(
     description="This is an example of using FastAPI"
 )
 
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # star means all client urls allowed 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 @app.get("/")           #endpoint, or route, always starts with a forward slash
 def default_route():    #route handler function
@@ -22,3 +15,16 @@ def default_route():    #route handler function
     """
     return "You have reached the default route. Back-end server is listening..."
     
+@app.get("/example")  
+def get_example():    
+    """
+    This endpoint returns a JSON object consisting of a simple message.
+    """
+    return {"message": "Hello World!"}
+
+@app.get("/example2")  
+def get_example2(name):    # can also pass in parameters
+    """
+    This endpoint takes in a parameter called "name"
+    """
+    return {"message": f"Hello {name}!"}
