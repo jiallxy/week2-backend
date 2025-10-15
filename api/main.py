c01c2e06e0ed9028fa707750ab50612d21713f9d
Justin Luo <87254766+jiallxy@users.noreply.github.com>
2025-10-15 19:23:51 -0400

    1

--- api/main.py
+++ api/main.py
@@ -22,16 +22,3 @@ def default_route():    #route handler function
     """
     return "You have reached the default route. Back-end server is listening..."
     
-@app.get("/example")  
-def get_example():    
-    """
-    This endpoint returns a JSON object consisting of a simple message.
-    """
-    return {"message": "Hello World!"}
-
-@app.get("/example2")  
-def get_example2(name):    # can also pass in parameters
-    """
-    This endpoint takes in a parameter called "name"
-    """
-    return {"message": f"Hello {name}!"}
